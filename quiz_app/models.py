71ac067e15467d7c153ba12d8c5dc6818511ab81
Andrei <buha2595@gmail.com>
2026-06-22 13:33:25 +0200

    feat[quiz_app]:add serializers

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+class Quiz(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="quizzes")
+    title = models.CharField(max_length=255, default="Generated Quiz")
+    description = models.TextField(blank=True, null=True)
+    video_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    question_title = models.TextField()
+    question_options = models.JSONField()
+
+    answer = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.quiz.title}- {self.question_title[:30]}"
