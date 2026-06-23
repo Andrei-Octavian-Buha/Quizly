f8f823ae3f499e684e5c85be06b74c6066b936b4
Andrei <buha2595@gmail.com>
2026-06-23 12:57:41 +0200

    feat[quiz_app]:add admin panel

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Quiz, Question
 
-# Register your models here.
+class QuestionInline(admin.TabularInline):
+    """
+    Allows questions to be edited directly inside the Quiz admin page.
+    """
+    model = Question
+    extra = 1  # Number of empty slots to display for new questions
+    fields = ['question_title', 'question_options', 'answer']
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Quiz model.
+    """
+    list_display = ['id', 'title', 'user', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'user']
+    search_fields = ['title', 'description']
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Question model (optional, for standalone viewing).
+    """
+    list_display = ['id', 'quiz', 'question_title', 'created_at']
+    list_filter = ['quiz', 'created_at']
+    search_fields = ['question_title']
